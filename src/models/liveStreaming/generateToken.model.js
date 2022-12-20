097c5b27a44125d9e3b5931c366c6190dd250096
bharathidev0303 <bharathiraja996574@gmail.com>
2022-12-20 13:26:46 +0530

    SD

--- src/models/liveStreaming/generateToken.model.js
+++ src/models/liveStreaming/generateToken.model.js
@@ -1,40 +1,44 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const { toJSON, paginate } = require('./plugins');
+const { toJSON, paginate } = require('../plugins');
 const { roles } = require('../../config/roles');
 const { StringDecoder } = require('string_decoder');
 const { v4 } = require('uuid');
 
-const userSchema = mongoose.Schema(
-  {
-    _id: {
-      type: String,
-      default: v4,
-    },
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    isEmailVerified: {
-      type: Boolean,
-      default: false,
-    },
-    resume: {
-      type: String,
-    },
-    workStatus: {
-      type: String,
-    },
-    mobileNumber: {
-      type: Number,
-    },
+const tempToken = mongoose.Schema({
+  _id: {
+    type: String,
+    default: v4,
   },
-  {
-    timestamps: true,
-  }
-);
+  token: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  created: {
+    type: Date,
+  },
+  time: {
+    type: Number,
+  },
+  expDate: {
+    type: Number,
+  },
+  created_num: {
+    type: Number,
+  },
+  participents: {
+    type: Number,
+  },
+  chennel: {
+    type: String,
+  },
+  Uid: {
+    type: Number,
+  },
+});
 
-const CandidateRegistration = mongoose.model('candidateRegistration', userSchema);
-module.exports = CandidateRegistration;
+const tempTokenModel = mongoose.model('tempToken', tempToken);
+module.exports = { tempTokenModel };
--- src/services/liveStreaming/generateToken.service.js
+++ src/services/liveStreaming/generateToken.service.js
@@ -2,6 +2,8 @@ const httpStatus = require('http-status');
 const ApiError = require('../../utils/ApiError');
 const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 const Agora = require('agora-access-token');
+const moment = require('moment');
+const { tempTokenModel } = require('../../models/liveStreaming/generateToken.model');
 
 const appID = '08bef39e0eb545338b0be104785c2ae1';
 const appCertificate = 'bfb596743d2b4414a1895ac2edb1d1f0';
@@ -10,11 +12,35 @@ const generateToken = async (req) => {
   const uid = req.query.uid;
   const role = req.body.isPublisher ? Agora.RtcRole.PUBLISHER : Agora.RtcRole.SUBSCRIBER;
   const channel = req.query.channel;
-  const currentTimestamp = Math.floor(Date.now() / 1000);
-  const expirationTimestamp = new Date(new Date('2022-12-20 11:51:59')).getTime() / 1000;
-
+  // const currentTimestamp = Math.floor(Date.now() / 1000);
+  // const expirationTimestamp = new Date(new Date('2022-12-20 11:51:59')).getTime() / 1000;
+  const moment_curr = moment();
+  const currentTimestamp = moment_curr.add(5, 'minutes');
+  const expirationTimestamp =
+    new Date(new Date(currentTimestamp.format('YYYY-MM-DD') + ' ' + currentTimestamp.format('HH:mm:ss'))).getTime() / 1000;
   const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, expirationTimestamp);
-  return { uid, token };
+  // console.log(
+  //   moment(new Date(new Date(currentTimestamp.format('YYYY-MM-DD') + ' ' + currentTimestamp.format('HH:mm:ss')))).format(
+  //     'YYYY-MM-DD HH:mm:ss'
+  //   )
+  // );
+  // console.log(
+  //   moment(new Date(new Date(moment().format('YYYY-MM-DD') + ' ' + moment().format('HH:mm:ss')))).format(
+  //     'YYYY-MM-DD HH:mm:ss'
+  //   )
+  // );
+  let value = await tempTokenModel.create({
+    token: token,
+    date: moment().format('YYYY-MM-DD'),
+    time: moment().format('HHMMSS'),
+    created: moment_curr,
+    Uid: uid,
+    chennel: channel,
+    participents: uid,
+    created_num: new Date(new Date(moment().format('YYYY-MM-DD') + ' ' + moment().format('HH:mm:ss'))).getTime(),
+    expDate: expirationTimestamp * 1000,
+  });
+  return { uid, token, value };
 };
 module.exports = {
   generateToken,
