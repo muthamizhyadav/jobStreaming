4ae884aa4229c4b85dec57c2f2b8d96f57828bd4
unknown <rohitkumar05999@gmail.com>
2022-12-20 17:29:21 +0530

    rgr

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -37,7 +37,7 @@ const defaultRoutes = [
     route: employerCandidateSearchRoute,
   },
   {
-    path: '/employerdetails',
+    path: '/employerdetail',
     route: employerdetailsRoute,
   },
 ];
