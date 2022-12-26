5d2926e99234cd842255db75c6508c8cfd7b8afb
unknown <rohitkumar05999@gmail.com>
2022-12-26 15:57:18 +0530

    35g5

--- src/services/employerDetails.service.js
+++ src/services/employerDetails.service.js
@@ -17,19 +17,66 @@ const createEmpDetails = async (userId, userBody) => {
 };
 
 const getByIdUser = async (id) => {
+
   const data = await EmployerDetails.aggregate([
     {
       $match: {
         $and: [{ userId: { $eq: id } }],
       },
     }, 
-    // {
-    //   $project:{
-    //     keySkill:1,
-
-    //   }
-    // }
-
+    {
+      $project:{
+        keySkill:1,
+        date:1,
+        active:1,
+        jobTittle:1,
+        designation:1,
+        recruiterName:1,
+        contactNumber:1,
+        jobDescription:1,
+        salaryRangeFrom:1,
+        salaryRangeTo:1,
+        experienceFrom:1,
+        experienceTo:1,
+        interviewType:1,
+        candidateDescription:1,
+        workplaceType:1,
+        industry:1,
+        interviewerName:1,
+        preferredindustry:1,
+        functionalArea:1,
+        role:1,
+        jobLocation:1,
+        employmentType:1,
+        openings:1,
+        interviewDate:1,
+        interviewTime:1,
+        location:1,
+        interviewerName:1,
+        interviewerContactNumber:1,
+        validity:1,
+        educationalQualification:1,
+        userId:1,
+       expiredDate:1,
+       createdAt:1,
+       adminStatus:{
+        $cond: {
+          if: {
+            $eq: ['$date', "$expiredDate"]
+          },
+          then:"Expired",
+          else:"$adminStatus",
+        }
+      },
+       
+      //  companyName:"$employerregistrations.companyName",
+      //  email:"$employerregistrations.email",
+      //  mobileNumber:"$employerregistrations.mobileNumber",
+      //  companyType:"$employerregistrations.companyType",
+      //  name:"$employerregistrations.name",
+      //  regitserStatus:"$employerregistrations.adminStatus",
+      }
+    }
   ])
   return data
 }
