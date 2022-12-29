0c32f1eb6812b9d74ddc79825cb9395f58b22d7b
unknown <rohitkumar05999@gmail.com>
2022-12-29 14:44:57 +0530

    efer

--- src/services/planPaymentDetails.service.js
+++ src/services/planPaymentDetails.service.js
@@ -14,7 +14,73 @@ const createPlanPayment = async (userId, userBody) => {
   return data;
 };
 
+const Plan_Deactivate = async (id) => {
+  const user = await PlanPayment.findById(id)
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'planPayment not found');
+  }
+  const data = await PlanPayment.findByIdAndUpdate({ _id: id }, {active:false}, { new: true });
+  await data.save();
+  return data;
+};
+
+const employerPlanHistory = async (id) => {
+  const data = await PlanPayment.aggregate([
+    { 
+      $match: { 
+        $and: [ { userId: { $eq: id } }] 
+    }
+    },
+    {
+      $lookup: {
+        from: 'createplans',
+        localField: 'planId',
+        foreignField: '_id',
+        as: 'createplans',
+      },
+    },
+    {
+      $unwind:'$createplans',
+    },
+    {
+      $lookup: {
+        from: 'employerdetails',
+        localField: 'userId',
+        foreignField: 'userId',
+        pipeline:[
+          {
+            $group: { _id: null, count: { $sum: 1 }, },
+          },
+        ],
+        as: 'employerdetails',
+      },
+    }, 
+    {
+      $unwind: {
+        path: '$employerdetails',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project:{
+        date:1,
+        planName:'$createplans.planName',
+        jobPost:'$createplans.jobPost',
+        cvAccess:'$createplans.cvAccess',
+        numberOfMassMailer:'$createplans.numberOfMassMailer',
+        cost:'$createplans.cost',
+        offer:'$createplans.offer',
+        validityOfPlan:'$createplans.validityOfPlan',
+        jobPostVAlidity:'$createplans.jobPostVAlidity',
+        postJobUsed:'$employerdetails.count'
+      }
+    }
+  ])
+  return data
+}
 
 module.exports = {
     createPlanPayment,
+    Plan_Deactivate,
+    employerPlanHistory,
 };
