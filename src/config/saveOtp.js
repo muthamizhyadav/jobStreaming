644ad799ff43236138978d725a6144e6bf682966
unknown <rohitkumar05999@gmail.com>
2023-01-04 12:45:28 +0530

    3rgr

--- src/config/saveOtp.js
+++ src/config/saveOtp.js
@@ -2,7 +2,13 @@ const {OTPModel} = require('../models');
 const moment = require('moment');
 
 const saveOtp = async (body, OTPCODE) => {
-    await OTPModel.create({otp: OTPCODE, mobileNumber:body.mobileNumber, userId:body._id});
+//  const data =  await OTPModel.findOne({mobileNumber:body.mobileNumber})
+//  if(data){
+//    await OTPModel.findOneAndUpdate({mobileNumber:body.mobileNumber}, {otp:OTPCODE}, {new:true})
+//  }else{
+//    await OTPModel.create({otp: OTPCODE, mobileNumber:body.mobileNumber, userId:body._id});
+//  }
+ await OTPModel.create({otp: OTPCODE, mobileNumber:body.mobileNumber, userId:body._id});
   };
 
   const UpdatesaveOtp = async (body, OTPCODE) => {
