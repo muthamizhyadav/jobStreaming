0480e07e03a6f3078110e32b0c266d7052331f87
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-01-04 14:11:01 +0530

    sd

--- src/app.js
+++ src/app.js
@@ -96,7 +96,14 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
   res.sendStatus(200);
 });
-
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+  if (req.method === "OPTIONS") res.send(200);
+  else next();
+}
+app.use(allowCrossDomain);
 // convert error to ApiError, if needed
 app.use(errorConverter);
 
